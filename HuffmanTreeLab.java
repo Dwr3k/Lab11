4e9c29e6a639798e63e3f3a0eb9858f1a4fd4a57
Dwr3k <mcgloneda@vcu.edu>
2019-04-10 06:14:02 -0400

    Add files via upload

--- HuffmanTreeLab.java
+++ HuffmanTreeLab.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class HuffmanTreeLab {
 
@@ -15,6 +17,23 @@ public class HuffmanTreeLab {
 			}
 		} 
 	}
+	
+	public static String[] makeString(Node root, String[] codes, String codeTracker)
+	{
+		
+		
+		if(root.left == null && root.right == null)
+		{
+			int index = Integer.valueOf(root.element);
+			codes[index] = codeTracker;
+			return codes;
+		}
+		
+		makeString(root.left, codes, codeTracker + "0");
+		makeString(root.right, codes, codeTracker + "1");
+		
+		return codes;
+	}
 
 	//	Get codes for each letter
 	public static String[] getCodes(Node root) {
@@ -29,9 +48,9 @@ public class HuffmanTreeLab {
 	// Assigns the Huffman code to each node in the Huffman Code Tree 
 	// and records the code in code array 
 	private static void assignCodes(Node root, String[] codes) 
-	{
-		// TODO 
-
+	{	
+		String codeTracker = "";
+		codes = makeString(root, codes, codeTracker);
 	}
 
 	/** Get a Huffman tree from the codes */  
@@ -39,49 +58,85 @@ public class HuffmanTreeLab {
 		// Create a Dictionary to hold the forest of trees
 		SortedLinkedDictionary<Integer, Node> forest = new SortedLinkedDictionary<>(); 
 
-		HuffmanTreeLab heck = new HuffmanTreeLab();
+		HuffmanTreeLab test = new HuffmanTreeLab();
 
-		for(int i = 0; i < counts.length; ++i) 
+		for(int i = 0; i < counts.length; ++i)
 		{
 			if(counts[i] != 0)
 			{
-				Node temp = heck.new Node(counts[i], (char) i);
-				forest.add(counts[i], temp);
+				forest.add(counts[i], test.new Node(counts[i], (char) i));
 			}
 		}
 
-		// TODO 
 		while(forest.getSize() != 1)
 		{
+			Node first = forest.remove(forest.getKeyIterator().next());
+			Node second = forest.remove(forest.getKeyIterator().next());
 			
+			int combinedWeight = first.weight + second.weight;
+
+			Node newTree = test.new Node(combinedWeight, String.valueOf(combinedWeight).charAt(0));
+			
+			newTree.weight = combinedWeight;
+			
+			if(first.compareTo(second) == 1)
+			{
+				newTree.left = first;
+				first.code = "0";
+				
+				newTree.right = second;
+				second.code = "1";
+			}
+			else if(first.compareTo(second) == 0)
+			{
+				newTree.left = first;
+				first.code = "0";
+				
+				newTree.right = second;
+				second.code = "1";
+			}
+			else
+			{
+				newTree.left = second;
+				second.code = "0";
+				
+				newTree.right = first;
+				first.code = "1";
+			}
+			
+			newTree.code = first.code + second.code;
+			
+			forest.add(combinedWeight, newTree);
 		}
 
+
+
 		// combine trees in the forest into a single tree
-		
+
 
 		return forest.remove(forest.getKeyIterator().next()); // The final tree
 	}
 
 	/** Get the frequency of the characters */
-	public static int[] getCharacterFrequency(String filename) 
-	{    
-		int counter = 0;
-		int[] charCounts = new int[256];
-		for(int i = 0; i < filename.length(); ++i)
-		{
-			for(int j = 0; j < filename.length(); ++j)
-			{
-				if(filename.charAt(j) == filename.charAt(i))
-				{
-					++counter;
-				}
-			}
+	public static int[] getCharacterFrequency(String filename) {    
+		int[] counts = new int[256]; // 256 ASCII characters
 
-			charCounts[(int) filename.charAt(i)] = counter;
+		try {
+			FileInputStream input = new FileInputStream(filename);
+			int inputByte = input.read();
+
+			while (inputByte != -1) {
+				if(Character.isLetter((char)inputByte))
+					counts[inputByte]++;
+				inputByte = input.read();
+			}
+			input.close();
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
 		}
 
-		return charCounts;
-
+		return counts;
 	}
 
 
